12b703ad3519841a381823afea74b24e89cc4c78
Nolan <60010605+nolngo@users.noreply.github.com>
2020-09-22 22:08:39 -0700

    revised typos

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 from random import choice    #this is needed to add the random functionality to python
 
-def scary_movie_bot(user_response):     #this is the bot's function, basically the brain of the bot where all the thinking and randomization happens
+def get_bot_response(user_response):     #this is the bot's function, basically the brain of the bot where all the thinking and randomization happens
 
   bot_response_supernatural = ["It's all fake! Watch a behind the scenes video!", "Go watch some peppa pig!"]
   bot_response_thriller = ["Stop watching thrillers! Pingu is a much more entertaining show", "The chances of that ever happenning to you is right around 0", "Go watch an episode of spongebob!"]
@@ -15,7 +15,7 @@ def scary_movie_bot(user_response):     #this is the bot's function, basically t
 
 
 print("Welcome to the scary movie recovery system")     #the welcoming statement upon opening the program
-print("Was the horror movie you watch supernatural or a thriller?")     #the prompt that requests an input
+print("Was the horror movie you watched supernatural or a thriller? ")     #the prompt that requests an input
 
 user_response = ""
 while True:     #boolean while loop, program will restart and run again unless the user inputs exit
@@ -25,5 +25,5 @@ while True:     #boolean while loop, program will restart and run again unless t
     break
 
   
-  bot_response = scary_movie_bot(user_response)     #this defines the bot's answer as the return in the function
+  bot_response = get_bot_response(user_response)     #this defines the bot's answer as the return in the function
   print(bot_response)       #this prints out the result of the bot's randomization based on user input
